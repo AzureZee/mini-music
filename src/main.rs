e7f3304a4f00eb448449e7608057078df914f45a
AzureZee <azureze@qq.com>
2025-09-08 11:07:14 +0800

    feat: 添加音乐播放功能
    - 使用 rodio 库实现音乐播放
    - 打开音频文件并解码
    - 创建音频流并播放音乐
    - 添加睡眠时间以保持程序运行

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use clap::Parser;
-use std::path::PathBuf;
+use rodio::{Decoder, OutputStreamBuilder, Sink};
+use std::{fs::File, path::PathBuf, thread, time};
 
 fn main() {
-    let arg = Cli::parse();
     println!("Music Player!");
+    let arg = Cli::parse();
     println!("File path: {:?}", arg.path);
+
+    // 获取对物理设备的输出流句柄
+    let stream_handle = OutputStreamBuilder::open_default_stream().unwrap();
+    let file = File::open(arg.path).unwrap();
+    // 创建一个新的接收器，并在流上开始播放。
+    let _sink = Sink::connect_new(&stream_handle.mixer());
+    let source = Decoder::try_from(file).unwrap();
+    stream_handle.mixer().add(source);
+
+    thread::sleep(time::Duration::from_secs(10));
 }
 
 #[derive(Parser)]
